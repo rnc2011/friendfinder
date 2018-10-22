2fc407690ef0fe3767367d0832d3d1caac81fd21
Richard <rncovington0455@gmail.com>
2018-10-22 16:56:35 -0400

    FriendFinder

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Robby",
+      photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350",
+      scores: [
+        "2",
+        "4",
+        "8",
+        "3",
+        "2",
+        "1",
+        "5",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Sally",
+      photo: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      scores: [
+        "4",
+        "2",
+        "5",
+        "1",
+        "2",
+        "2",
+        "9",
+        "1",
+        "3",
+        "1"
+      ]
+    },
+    {
+      name: "Aaron",
+      photo: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      scores: [
+        "5",
+        "2",
+        "2",
+        "2",
+        "4",
+        "1",
+        "3",
+        "2",
+        "5",
+        "5"
+      ]
+    },
+    {
+      name: "Elizabeth",
+      photo: "https://images.pexels.com/photos/324658/pexels-photo-324658.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      scores: [
+        "3",
+        "3",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "2",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Cynthia",
+      photo: "https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      scores: [
+        "4",
+        "3",
+        "4",
+        "1",
+        "5",
+        "2",
+        "5",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Alex",
+      photo: "https://images.pexels.com/photos/213117/pexels-photo-213117.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      scores: [
+        "4",
+        "4",
+        "2",
+        "3",
+        "2",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+  
+  module.exports = friends;
+  
